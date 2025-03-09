0f70078f7aad627bcd939b1c7591f015e491f49e
aqsasafdar530 <safdaraqsa9@gmail.com>
2025-03-09 11:19:03 +0000

    calculating average time

--- ASSIGNMENT-1146/q2dynamic.c
+++ ASSIGNMENT-1146/q2dynamic.c
@@ -34,3 +34,11 @@ int main() {
 
         printf("Run %d: Sum = %.2f, Time = %f sec\n", j + 1, sum, time);
     }
+    // Compute and display average execution time
+    avg_time /= 10.0;
+    printf("\n[DYNAMIC SCHEDULING] Average Time over 10 runs = %f sec\n", avg_time);
+
+    // Free allocated memory
+    free(arr);
+    return 0;
+}
